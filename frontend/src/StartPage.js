362c25a3cbf294874e20098ebeaf449ef6c6de17
TheNathanSpace <46632454+TheNathanSpace@users.noreply.github.com>
2024-04-20 14:49:14 -0600

    Created text input component

--- frontend/src/StartPage.js
+++ frontend/src/StartPage.js
@@ -1,30 +1,61 @@
-import React from 'react';
+import React, {useRef, useState} from 'react';
 import {useLocation} from "wouter";
+import ValidationError from "./ValidationError";
+import TextInput from "./TextInput";
 
 const StartPage = () => {
     const [, setLocation] = useLocation();
+    const [errors, setErrors] = useState()
+
+    const playlistValueRef = useRef("");
+    const usernameValueRef = useRef("");
+    const passwordValueRef = useRef("");
+
+
     const handleStart = () => {
-        setLocation("/playlist");
+        /*
+         TODO:
+            1. Handle input validation
+            2. Route to /playlist, injecting with input
+            3. Playlist page take input, gets data from back-end, displays it
+         */
+        const newErrors = [];
+        if (!playlistValueRef.current) {
+            newErrors.push(<ValidationError field={"Playlist URL"}
+                                            message={"Please enter valid Spotify playlist URL"}
+                                            key={"Playlist URL"}/>);
+        }
+        if (!usernameValueRef.current) {
+            newErrors.push(<ValidationError field={"Username"}
+                                            message={"Please enter username"}
+                                            key={"Username"}/>);
+
+        }
+        if (!passwordValueRef.current) {
+            newErrors.push(<ValidationError field={"Password"}
+                                            message={"Incorrect password"}
+                                            key={"Password"}/>);
+
+        }
+        if (playlistValueRef.current && usernameValueRef.current && passwordValueRef.current) {
+            setLocation("/playlist");
+        } else {
+            setErrors(newErrors);
+        }
     };
 
-    return (
-        <div>
+    return (<div>
             <h1>Spotify Playlist Ranker</h1>
-            <div className={"vert-centered"}>
-                <p className={"inline-child"}>Enter playlist URL:</p><input type={"text"}
-                                                                            className={"inline-child"}/>
-            </div>
-            <div className={"vert-centered"}>
-                <p className={"inline-child"}>Username:</p><input type={"text"} className={"inline-child"}/>
-            </div>
-            <div className={"vert-centered"}>
-                <p className={"inline-child"}>Passcode:</p><input type={"text"} className={"inline-child"}/>
-            </div>
+            <TextInput prompt={"Enter playlist URL:"} valueRef={playlistValueRef}/>
+            <TextInput prompt={"Username:"} valueRef={usernameValueRef}/>
+            <TextInput prompt={"Password:"} valueRef={passwordValueRef}/>
+
             <div className={"hor-centered"}>
                 <div className={"inline-child"}>
                     <button onClick={handleStart}>Start!</button>
                 </div>
             </div>
+            {errors}
         </div>
     );
 }
