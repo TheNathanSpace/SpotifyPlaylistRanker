9f3d7f13171fd80a5808386e04c7c4bcff66ea94
TheNathanSpace <nathan@thenathan.space>
2024-05-16 19:56:45 -0600

    Fixed ranking toggle

--- frontend/src/PlaylistPage.js
+++ frontend/src/PlaylistPage.js
@@ -1,12 +1,13 @@
-import React, {useEffect, useRef, useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import greyImage from "./images/grey.png"
-import {Button, Divider} from "@mui/joy";
-import {useLocation, useParams, useRoute} from "wouter";
+import {Divider} from "@mui/joy";
+import {useParams} from "wouter";
 import {GET_PLAYLIST_DATA} from "./addresses";
 import PropTypes from "prop-types";
+import ToggleRankingButton from "./ToggleRankingButton";
 
 const PlaylistPage = (props) => {
-    const [location, setLocation] = useLocation();
+    const params = useParams();
 
     const [mouseInProfile, setMouseInProfile] = useState(false);
     const [mouseInPlaylist, setMouseInPlaylist] = useState(false);
@@ -21,13 +22,6 @@ const PlaylistPage = (props) => {
         profile_image: greyImage
     });
 
-    useEffect(()=>{
-        /*
-        Query logic
-        */
-        console.log('i fire once');
-    },[]);
-
     /*
        Use useEffect hook with no dependencies to fetch playlist data on initial load.
     */
@@ -35,12 +29,12 @@ const PlaylistPage = (props) => {
         // This is so dumb https://stackoverflow.com/a/64079172/7492795
         (async () => {
             const urlParams = {
-                playlist_uri: props.playlist_uri
+                playlist_uri: params.playlist_uri
             }
             const target = GET_PLAYLIST_DATA + "?" + new URLSearchParams(urlParams).toString();
             const response = await (await fetch(target)).json();
             setPlaylistData({
-                playlist_uri: props.playlist_uri,
+                playlist_uri: params.playlist_uri,
                 playlist_name: response.playlist_name,
                 playlist_description: response.playlist_description,
                 playlist_image: response.playlist_image,
@@ -48,7 +42,6 @@ const PlaylistPage = (props) => {
                 profile_username: response.profile_username,
                 profile_image: response.profile_image
             })
-            console.log("Playlist data:", response)
         })();
     }, []);
 
@@ -60,12 +53,8 @@ const PlaylistPage = (props) => {
         }).click();
     }
 
-    const toggleRanking = () => {
-        setLocation("~" + props.buttonTarget.replace("%s", props.playlist_uri))
-    }
-
     const getPlaylistURL = () => {
-        return "https://open.spotify.com/playlist/" + props.playlist_uri;
+        return "https://open.spotify.com/playlist/" + params.playlist_uri;
     }
 
     /*
@@ -121,16 +110,9 @@ const PlaylistPage = (props) => {
 
                 </div>
                 <div className={"playlist-info-column"}>
-                    <Button
-                        className={"start-ranking-button"}
-                        color="primary"
-                        variant="solid"
-                        onClick={() => {
-                            toggleRanking()
-                        }}
-                    >
-                        {props.buttonLabel}
-                    </Button>
+                    <ToggleRankingButton
+                        setIsRanking={props.setIsRanking}
+                    />
                 </div>
             </div>
             <div className={"divider"}>
@@ -141,9 +123,7 @@ const PlaylistPage = (props) => {
 }
 
 PlaylistPage.props = {
-    playlist_uri: PropTypes.string,
-    buttonLabel: PropTypes.string,
-    buttonTarget: PropTypes.string
+    setIsRanking: PropTypes.any,
 }
 
 export default PlaylistPage;
