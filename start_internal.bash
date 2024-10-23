02f78f5b59a7a3f071e29194439b1677c008c009
TheNathanSpace <nathan@thenathan.space>
2024-10-23 17:30:56 -0600

    fix

--- start_internal.bash
+++ start_internal.bash
@@ -2,13 +2,18 @@
 
 export $(xargs </app/.env)
 
+echo "backend: $backend_port"
+echo "frontend: $REACT_APP_PORT"
+
 if [ -z "${REACT_APP_PORT}" ]; then
   REACT_APP_PORT=3000
   export REACT_APP_PORT
 fi
 
 if [ ! -f ".env" ]; then
-  ln -s .env frontend/.env
+  ln -s /app/.env /app/frontend/.env
 fi
+
+export REACT_APP_BACKEND_PORT="$backend_port"
 
 (cd /app/backend && python app.py) & (cd /app/frontend && REACT_APP_BACKEND_PORT="$backend_port" npm start -- --port "$REACT_APP_PORT") & wait -n
